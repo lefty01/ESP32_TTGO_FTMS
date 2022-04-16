331116616553f1a960f0778281111142ee53f6cf
Zingo Andersen <zingo@zingo.org>
2022-04-16 21:04:06 +0200

    Update  Copyright to remove  2021 and and Zingo on new AW9523 files

--- src/GPIOExtenderAW9523.cpp
+++ src/GPIOExtenderAW9523.cpp
@@ -2,7 +2,7 @@
  *
  *
  * The MIT License (MIT)
- * Copyright © 2021, 2022 <Andreas Loeffler>
+ * Copyright © 2022 <Andreas Loeffler> <Zingo Andersen>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the “Software”), to deal in the Software without
