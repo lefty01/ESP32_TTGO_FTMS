ef4cc88a30dd1a8e461db8eda680c85dae2099e3
andreas loeffler <al@exitzero.de>
2026-01-04 01:04:14 +0100

    fix coding style

--- src/display.cpp
+++ src/display.cpp
@@ -4,28 +4,31 @@
  * The MIT License (MIT)
  * Copyright © 2021, 2022, 2025, 2026 <Andreas Loeffler>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- * and associated documentation files (the “Software”), to deal in the Software without
- * restriction, including without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#include <Arduino.h>
+#include "display.h"
 #include "common.h"
+#include "config.h"
 #include "debug_print.h"
 #include "hardware.h"
-#include "display.h"
-#include "config.h"
+#include <Arduino.h>
 
 #ifndef NO_DISPLAY
 #if LGFX_USE_V1
@@ -89,7 +92,6 @@ void gfxLogText(const char *text)
 #endif
 }
 
-
 void gfxUpdateDisplay(bool clear)
 {
 #ifndef NO_DISPLAY
@@ -102,8 +104,6 @@ void gfxUpdateDisplay(bool clear)
 #endif
 }
 
-
-
 void gfxUpdateHeader()
 {
 #ifndef NO_DISPLAY
@@ -115,4 +115,3 @@ void gfxUpdateHeader()
 
 #endif
 }
-
